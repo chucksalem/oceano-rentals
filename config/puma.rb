dadc8f7683a00577ce84ff6b9ab0b1e4fe1917a8
Kirill Karpinovsky <karpinovsky@gmail.com>
2019-03-07 02:50:18 +0300

    Revert puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,3 +2,12 @@
 
 port        ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
+
+workers 2
+preload_app!
+
+bind "unix://tmp/puma.sock"
+
+daemonize true
+
+pidfile 'tmp/pids/puma.pid'
