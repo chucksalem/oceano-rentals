1e78fd248f386e836bbcbcf8d485f3cd4e97c9a5
Kirill Karpinovsky <karpinovsky@gmail.com>
2019-03-18 21:06:26 +0300

    Specify rails env for tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@ project_dir = File.expand_path(File.dirname(__FILE__) + '/..')
 set :output, "#{path}/log/crontab.log"
 
 every :hour do
-  command "cd #{project_dir} && ~/.rvm/bin/rvm default do bundle exec rake oceano:cache:properties"
-  command "cd #{project_dir} && ~/.rvm/bin/rvm default do bundle exec rake oceano:cache:weather"
+  command "cd #{project_dir} && RAILS_ENV=#{ENV['RAILS_ENV']} bundle exec rake oceano:cache:properties"
+  command "cd #{project_dir} && RAILS_ENV=#{ENV['RAILS_ENV']} bundle exec rake oceano:cache:weather"
 end
